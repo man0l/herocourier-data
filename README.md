33e9f2ef24d98fda1cebf751bd366823da6263de
Manol Trendafilov <manol.trendafilov@gmail.com>
2020-04-17 13:11:28 +0300

    setup windows running environment for headless chrome

--- README.md
+++ README.md
@@ -4,3 +4,14 @@
 
 `docker run herocourier node index.js`
 
+## windows run
+`npm run start`
+
+Nick.js runs child process and it is looking for env variable process.env.CHROME_PATH. I use chrome-finder library to find the path to chrome.
+
+```javascript
+ const findChrome = require("chrome-finder");
+ let chromePath  = findChrome();
+ 
+ process.env.CHROME_PATH = chromePath;
+```
--- index.js
+++ index.js
@@ -2,12 +2,16 @@
 
 const Nick = require("nickjs");
 const utils = require("./utils/utils");
+const findChrome = require("chrome-finder");
 
 const nick = new Nick({
     userAgent: "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/53.0.2785.143 Safari/537.36",
 });
 
 let seedUrlFile = './seed-urls.txt';
+let chromePath  = findChrome();
+
+console.log(chromePath);
 
 (async () => {
 
@@ -15,6 +19,15 @@ let seedUrlFile = './seed-urls.txt';
     lines.forEach(async (line) => {
         const tab = await nick.newTab();
         tab.open(line);
+
+        console.log('Injecting jQuery...');
+        await tab.inject('https://code.jquery.com/jquery-3.1.1.slim.min.js');
+
+        const title = await tab.evaluate((arg, done) => {
+            done(null, jQuery('title').text())
+        });
+
+        console.log('The title is: ' + title);
         tab.close();
     });
 })()
